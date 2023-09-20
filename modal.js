6a5c96a2ce1ea1fa87789563b162c7a6e290a7a4
Aeromkana <hayward.lance@gmail.com>
2023-09-20 09:38:29 +0100

    Toggle Modal

--- modal.js
+++ modal.js
@@ -9,15 +9,22 @@ function editNav() {
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
+const modalBtn = document.querySelectorAll(".modal-btn, .close"); //my mod
 const formData = document.querySelectorAll(".formData");
 
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+// // launch modal event
+// modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
+// // launch modal form
+// function launchModal() {
+//   modalbg.style.display = "block";
+// }
+
+//My code
+// Toggle Modal Function
+function toggleModal() {
+  modalbg.classList.toggle("display-block")
 }
 
-
+//Toggle Modal Event
+modalBtn.forEach((btn) => btn.addEventListener("click", toggleModal))
