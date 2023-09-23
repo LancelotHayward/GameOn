ae6d854c93189ce6659f896502a614bdd2aefb52
Aeromkana <hayward.lance@gmail.com>
2023-09-23 07:41:48 +0100

    Started form entry (refactored that part of HTML)

--- modal.js
+++ modal.js
@@ -1,10 +1,10 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
 }
 
 // DOM Elements
@@ -12,19 +12,16 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn, .close"); //my mod
 const formData = document.querySelectorAll(".formData");
 
-// // launch modal event
-// modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-// // launch modal form
-// function launchModal() {
-//   modalbg.style.display = "block";
-// }
-
 //My code
 // Toggle Modal Function
 function toggleModal() {
-  modalbg.classList.toggle("display-block")
+    modalbg.classList.toggle("display-block")
 }
 
 //Toggle Modal Event
 modalBtn.forEach((btn) => btn.addEventListener("click", toggleModal))
+
+//Form entry
+function submitForm() {
+    formData.forEach((entry) => console.log(entry.getElementsByTagName("input")))
+}
