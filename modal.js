73409874cc1ed83bfa1df964bf4dc1a5014ab7c8
LancelotHayward <hayward.lance@gmail.com>
2023-09-28 10:07:58 +0200

    submitForm function started

--- modal.js
+++ modal.js
@@ -21,7 +21,41 @@ function toggleModal() {
 //Toggle Modal Event
 modalBtn.forEach((btn) => btn.addEventListener("click", toggleModal))
 
+//Form Submit
+document.getElementsByTagName("form")[0].addEventListener('submit', (e) => {
+    e.preventDefault()
+})
 //Form entry
 function submitForm() {
-    formData.forEach((entry) => console.log(entry.getElementsByTagName("input")[0].value))
+    // formData.forEach((entry) => console.log(entry.getElementsByTagName("input")[0].value))
+    errorMessage = ""
+    if (document.getElementById("first").value.length < 2) {
+        errorMessage += " first"
+    }
+    if (document.getElementById("last").value.length < 2) {
+        errorMessage += " last"
+    }
+    if (document.getElementById("quantity").value === "") {
+        errorMessage += " quantity"
+    }
+    if (document.getElementById("email").value === "") {
+        errorMessage += " email"
+    }
+    checkBoxes = document.getElementsByClassName("checkbox-input")
+    locationCheck = false
+    Array.from(checkBoxes).forEach(checkBox =>  {
+            if (checkBox.type == "radio" && checkBox.checked) {
+                locationCheck = true
+            }
+        })
+    if (!locationCheck) {
+        errorMessage += " location"
+    }
+    if (!document.getElementById("checkbox1").checked) {
+        errorMessage += " terms"
+    }
+    if (errorMessage != "") {
+        errorMessage = "Errors:" + errorMessage
+        return errorMessage
+    }
 }
